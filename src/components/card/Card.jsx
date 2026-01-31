00cfec842088e3c246647f4e186c491704680026
shreyagargg <shreyagarg06031412@gmail.com>
2026-01-31 23:55:14 +0530

    frontend components

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from './card.module.css';
+
+function Card(){
+    return (
+        <div className= {style.card}>
+
+        </div>
+    )
+}
+
+export default Card;
